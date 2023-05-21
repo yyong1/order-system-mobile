a38b66e7650bfd407446d510a7192ca6df5545a4
Andrey Baranov <baranov.a.64103@gmail.com>
2023-05-21 18:51:05 +0200

    first part home page

--- app/src/main/java/com/example/foodorder/MainActivity.kt
+++ app/src/main/java/com/example/foodorder/MainActivity.kt
@@ -3,22 +3,25 @@ package com.example.foodorder
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.foodorder.ui.theme.*
 
 
@@ -32,38 +35,93 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
-fun HomeScreen(){
+fun HomeScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(start = 30.dp, top = 48.dp, end = 17.dp)
-    ){
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            BoxWithRes(resId = R.drawable.menu, description = "Menu")
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Icon(
-                    painter = painterResource(id = R.drawable.location),
-                    contentDescription = "Location",
-                    modifier = Modifier.size(16.dp),
-                    tint = Orange500
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(text = "California, US") // Should be dynamic rendering
-                Spacer(modifier = Modifier.width(8.dp))
-                Icon(
-                    painter = painterResource(id = R.drawable.arrow_down),
-                    contentDescription = "Down",
-                    modifier = Modifier.size(16.dp),
-                    tint = Orange500
-                )
+    ) {
+        Column {
+            Header()
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(156.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(Yellow200)
+                    .padding(24.dp)
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(verticalArrangement = Arrangement.SpaceBetween) {
+                        Text(buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = BlackTextColor)) {
+                                append("The fastest in\n" + "Delivery")
+                            }
+                            withStyle(style = SpanStyle(color = Yellow500)) {
+                                append("The fastest in\n" + "Delivery")
+                            }
+                        })
+                        Box(
+                            modifier = Modifier
+                                .size(width = 126.dp, height = 40.dp)
+                                .clip(RoundedCornerShape(10.dp))
+                                .background(Yellow500),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = "Order Now",
+                                style = Typography.body1,
+                                fontWeight = FontWeight.SemiBold,
+                                color = Color.White,
+                                fontSize = 14.sp
+                            )
+                        }
+                    }
+                    Image(
+                        painter = painterResource(id = R.drawable.deliveryman),
+                        contentDescription = "Deliveryman",
+                        modifier = Modifier.size(156.dp)
+                    )
+                }
             }
-            BoxWithRes(resId = R.drawable.search, description = "Search")
         }
+    }
+}
+
+@Composable
+fun Header() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        BoxWithRes(resId = R.drawable.menu, description = "Menu")
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                painter = painterResource(id = R.drawable.location),
+                contentDescription = "Location",
+                modifier = Modifier.size(16.dp),
+                tint = Orange500
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "California, US") // Should be dynamic rendering
+            Spacer(modifier = Modifier.width(8.dp))
+            Icon(
+                painter = painterResource(id = R.drawable.arrow_down),
+                contentDescription = "Down",
+                modifier = Modifier.size(16.dp),
+                tint = Orange500
+            )
+        }
+        BoxWithRes(resId = R.drawable.search, description = "Search")
     }
 }
 
@@ -75,14 +133,14 @@ fun BoxWithRes(
     iconColor: Color? = IconColor,
     boxSize: Int? = 40,
     iconSize: Int = 24
-){
+) {
     Box(
         modifier = Modifier
             .size(boxSize!!.dp)
             .clip(RoundedCornerShape(10.dp))
             .background(bgColor!!),
         contentAlignment = Alignment.Center
-    ){
+    ) {
         Icon(
             painter = painterResource(id = resId),
             contentDescription = description,
