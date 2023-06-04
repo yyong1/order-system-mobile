710930307315cb3f25cd07255cd215d77ecb6102
Andrey Baranov <baranov.a.64103@gmail.com>
2023-06-04 21:09:51 +0200

    reg fix

--- app/src/main/java/com/example/foodorder/data/models/User.kt
+++ app/src/main/java/com/example/foodorder/data/models/User.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "users")
 data class User(
     @PrimaryKey(autoGenerate = true)
-    val id: Int? = 0,
+    val id: Int? = null,
     val name: String,
     val email: String,
     val password: String
